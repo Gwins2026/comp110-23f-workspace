804037f1283966315ec45e46a8d44730ef339ac6
Georgia Winston <gwins26@unc.edu>
2023-09-20 23:10:04 -0400

    Progress on EX02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,47 @@
+"""EX02 - One-Shot Wordle"""
+
+__author__ = "730561330"
+
+secret: str = "python"
+
+# prompts user for their guess
+guess: str = input(f"What is your {len(secret)}-letter guess? ")
+str(guess)
+
+# makes sure the lengths of the word guessed and the secret word are the same
+while len(guess) != len(secret):
+        guess = str(input(f"That was not {len(secret)} letters! Try again: "))
+ 
+guess_idx: int = 0
+blocks: str = ""
+white_box: str = "\U00002B1C"
+green_box: str = "\U0001F7E9"
+yellow_box: str = "\U0001F7E8"
+
+# goes through each letter of the guessed word
+while guess_idx < len(secret):
+    # checks to see if the guessed word is the same as the secret word
+    if guess[guess_idx] == secret[guess_idx]:
+        blocks += green_box
+    else:
+        character_present: bool = False
+        alt_idx: int = 0
+        # goes through every letter of the secret word and checks for the guessed letter
+        while character_present is False and alt_idx < len(secret):
+            if guess[guess_idx] == secret[alt_idx]:
+                character_present = True
+            else:
+                alt_idx += 1
+        # adds a yellow block (correct, but wrong place) or white (incorrect) block to block string 
+        if guess_idx is True:
+            blocks += yellow_box
+        else:
+            blocks += white_box
+    guess_idx += 1
+print(blocks)
+
+# tells user if they guessed the word correctly or not
+if guess == secret:
+    print("Woo! You got it!")
+else:
+    print("Not quite play again soon!")
