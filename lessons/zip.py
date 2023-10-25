168d43b68222bc550dd11cf5f59f4ed46c848f75
Georgia Winston <gwins26@unc.edu>
2023-10-24 22:08:22 -0400

    got it

--- lessons/zip.py
+++ lessons/zip.py
@@ -3,7 +3,7 @@
 __author__ = "730561330"
 
 
-def zip(list_1: list[str]), list_2: list[int]) -> dict[str, int]:
+def zip(list_1: list[str], list_2: list[int]) -> dict[str, int]:
     """Creates a dictionary with keys as strings from list_1 and values as integers from list_2."""
     if len(list_1) != len(list_2) or (len(list_1) == 0):
         return {}
