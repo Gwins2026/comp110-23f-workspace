7265fe1dc5c50f77fcbe2d62aa2b169ba8933b88
Georgia Winston <gwins26@unc.edu>
2023-10-26 23:27:23 -0400

    g

--- lessons/zip_test.py
+++ lessons/zip_test.py
@@ -5,21 +5,21 @@ __author__ = "730561330"
 from lessons.zip import zip
 
 
-def test_multiple_char():
+def test_multiple_char() -> None:
     """Tests entries with multiple characters in the lists."""
     test_list_1: list[str] = ["shhh", "bang", "g"]
-    test_list_2: list[int] = ["5", "116", "88"]
+    test_list_2: list[int] = [5, 116, 88]
     assert zip(test_list_1, test_list_2) == {"shhh": 5, "bang": 116, "g": 88}
 
 
-def test_empty_list():
+def test_empty_list() -> None:
     """Tests if when the list unequal list lengths are given, the zipped product produces an error."""
     test_list_1: list[str] = ["g", "e", "o"]
     test_list_2: list[int] = [1, 6]
     assert zip(test_list_1, test_list_2) == {}
 
 
-def test_neg():
+def test_neg() -> None:
     """Tests negatives in the integer list."""
     test_list_1: list[str] = ["b", "a", "g"]
     test_list_2: list[int] = [-1, -2, -3]
