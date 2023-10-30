c7f8c57badc08bc7c0d84dde28f89c1f021d15f4
Georgia Winston <gwins26@unc.edu>
2023-10-30 14:12:06 -0400

    for now save

---
+++ exercises/ex06/dictionary.py
@@ -0,0 +1,36 @@
+"""EX06 - Practice using Dictionary Functions."""
+
+__author__ = "730561330"
+
+
+def invert(dicts: dict[str, str]) -> dict[str, str]:
+    """A given dictionary is inverted and the key list becomes the values of the output list and vice versa."""
+    if 
+        raise KeyError("A given list value repeats itself.")
+    
+
+def favorite_color(dicts: dict[str, str]) -> str:
+    """In a given dictionary of people and their favorite color, function determines which color is most popular."""
+
+
+def count(freq_list: list[str]) -> dict[str, int]:
+    """Given a list of strings, function produces a dictionary accourding to frequency of each value in input list."""
+    result: dict[str, int] = {}
+    i: int = 0
+    while i < leng(freq_list):
+        if freq_list[i] in result:
+            # add 1 to int list
+            i += 1
+        else:
+            # add variable to key list and 1 to int list
+            1 += 1
+    return result
+
+
+def alphabetizer(cate_list: list[str]) -> dict[str, list[str]]:
+    """Given a list of strings, function cataegorizes list into an order according to first letter in the string."""
+    result: dict[str, list[str]] = {}
+
+
+def update_attendance(attend_log: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """Given a dictionary of students and their class attendance, function updates and changes that dictionary to reflect new attendance by students."""
