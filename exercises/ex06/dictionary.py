f611aacf5f8882f6695f833cd7a52633714592d0
Georgia Winston <gwins26@unc.edu>
2023-11-01 21:52:30 -0400

    hopeful

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -5,32 +5,68 @@ __author__ = "730561330"
 
 def invert(dicts: dict[str, str]) -> dict[str, str]:
     """A given dictionary is inverted and the key list becomes the values of the output list and vice versa."""
-    if 
+    if dicts[key] in dicts > 1:
         raise KeyError("A given list value repeats itself.")
-    
+    inverted: dict[str, str] = {}
+    for key in dicts:
+        inverted() += {dicts[key]: key}
+    return inverted 
 
-def favorite_color(dicts: dict[str, str]) -> str:
+
+def favorite_color(colors: dict[str, str]) -> str:
     """In a given dictionary of people and their favorite color, function determines which color is most popular."""
+    color_counts: dict[str, str] = {}
+    first_appearance: dict[str, str]: {}
+    fav_color: str = None
+    i: int = 0
+    for name, color in colors[name]:
+        if color in color_counts:
+            color_counts[color] += 1
+        else:
+            color_counts[color] = 1
+            first appearance[color] = name
+        if color_counts[color] > i:
+            i = color_counts[color]
+            fav_color = color
+        elif color_counts[color] == i:
+            if name == first appearance[color]:
+                fav_color = color
+    return fav_color 
 
 
 def count(freq_list: list[str]) -> dict[str, int]:
-    """Given a list of strings, function produces a dictionary accourding to frequency of each value in input list."""
+    """Given a list of strings, function produces a dictionary according to frequency of each value in input list."""
     result: dict[str, int] = {}
     i: int = 0
-    while i < leng(freq_list):
+    while i < len(freq_list):
         if freq_list[i] in result:
-            # add 1 to int list
+            result[i] += 1
             i += 1
         else:
-            # add variable to key list and 1 to int list
-            1 += 1
-    return result
+            result{} += {freq_list[i]: 1}
+            i += 1
+    return result 
 
 
 def alphabetizer(cate_list: list[str]) -> dict[str, list[str]]:
     """Given a list of strings, function cataegorizes list into an order according to first letter in the string."""
     result: dict[str, list[str]] = {}
+    cate_list.lower()
+    i: int = 0
+    while i < len(cate_list):
+        if cate_list[i[0]] in result:
+            result[cate_list[i[0]]] += cate_list[i]
+        else:
+            result() += {cate_list[i[0]]: cate_list[i]}
+        i += 1
+    return result 
 
 
 def update_attendance(attend_log: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
     """Given a dictionary of students and their class attendance, function updates and changes that dictionary to reflect new attendance by students."""
+    for key in attend_log:
+        if key == day:
+            attend_log[key] += student
+        else: 
+            attend_log() += {day: student}
+    return attend_log 
