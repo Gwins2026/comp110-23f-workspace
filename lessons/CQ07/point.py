32a83b60a1d33a07a4ec0f7c3b9f213475a76eaf
Georgia Winston <gwins26@unc.edu>
2023-11-06 23:04:30 -0500

    got it

---
+++ lessons/CQ07/point.py
@@ -0,0 +1,26 @@
+""" """
+
+__author__ = "730561330"
+
+from __future__ import annotations
+
+
+class Point:
+    """Represents a point on the 2-D Cartesian plane."""
+    x: float
+    y: float
+
+    def __init__(self, x_init: float, y_init: float):
+        """Point's constructor."""
+        self.x = x_init
+        self.y - y_init
+
+    def scale_by(self, factor: int):
+        """Mutates point by scaling x and y attributes by 'factor'."""
+        self.x *= factor
+        self.y *= factor
+
+    def scale(sefl, factor: int) -> Point:
+        """Returns a new point with factor-scaled attributes."""
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
