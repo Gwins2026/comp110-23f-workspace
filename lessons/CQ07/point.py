7e52d904d14cd28b58d7501e01a7deb958ebf0cf
Georgia Winston <gwins26@unc.edu>
2023-11-14 22:52:04 -0500

    g

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -13,7 +13,23 @@ class Point:
         """Point's constructor."""
         self.x = x_init
         self.y = y_init
-
+    
+    def __str__(self) -> str:
+        """Returns a string of the point's coordinates."""
+        output: str = f"x: {self.x}; y: {self.y}"
+        return output
+    
+    def __mul__(self, factor: int | float) -> Point:
+        """Returns a new point with factor scaled attributes."""
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+    def __add__(self, factor: int | float) -> Point:
+        """Returns a new point with attributes added to a factor."""
+        new_point: Point = Point(self.x + factor, self.y + factor)
+        return new_point
+    
+    # CQ07
     def scale_by(self, factor: int):
         """Mutates point by scaling x and y attributes by 'factor'."""
         self.x *= factor
